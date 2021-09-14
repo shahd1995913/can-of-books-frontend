40e0066e2563fe150d19b05f3f984b60d15223cd
shahd1995913 <shahdalkhatib95@gmail.com>
2021-09-14 13:52:56 +0300

    update1

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -11,7 +11,7 @@ class MyFavoriteBooks extends React.Component {
 
     super(props);
     this.state = {
-    FavBookArr: []
+      FavBookArr: []
 
     }
 
@@ -29,70 +29,113 @@ class MyFavoriteBooks extends React.Component {
 
     })
 
-    // catch(err=>{
+      .catch(err => {
 
 
-    // console.log(err + 'Error');
+        console.log(err + 'Error');
 
-    // })
+      })
 
   }
 
 
-addBook=(event)=>{
-event.preventDefault();
-const{user}=this.props.auth0;
-const email =user.email;
-const obj={
+  addBook = (event) => {
+    event.preventDefault();
+    const { user } = this.props.auth0;
+    const email = user.email;
+    const obj = {
 
-  BookName:event.target.BookName.value,
-  Description:event.target.Description.value,
-  Status:event.target.Status.value,
-  ownerEmail:email
+      BookName: event.target.BookName.value,
+      Description: event.target.Description.value,
+      Status: event.target.Status.value,
+      ownerEmail: email
 
-}
+    }
+
+    axios.post(`http://localhost:3010/addBook`, obj).then(result => {
+
+      this.setState({ FavBookArr: result.data })
 
 
-// we need the POST to send data and the data secoure the data in body must to be not in the header
-//  POST-->Add  for security put the data inside the body no one can acess expect the authorized
-// When send data use POST the data will not be in Qury String Parameter QSP
-// the GET data will be exist in the QSP
-}
+    })
+
+      .catch(err => {
+
+        console.log("Error on Adding data");
+
+      })
+
+  }
+
+  // we need the POST to send data and the data secoure the data in body must to be not in the header
+  //  POST-->Add  for security put the data inside the body no one can acess expect the authorized
+  // When send data use POST the data will not be in Qury String Parameter QSP
+  // the GET data will be exist in the QSP
+
+
+
+  deleteBook = (id) => {
+    // how to delete a specsifi item from DS  
+    // delete like Get  i can acess by ? in URL 
+    //PUT like Post
+    const { user } = this.props.auth0;
+    const email = user.email;
+    axios.delete(`http://localhost:3010/deleteBook/${id}?email=${email}`)
+      .then(result => {
+        this.setState({
+          FavBookArr: result.data
+        })
+      })
+      .catch(err => {
+        console.log('error in deleting Book');
+      })
+  }
+
+  // i need to update the array after make a delete item 
+
+  // console.log(id)
+
+
+
 
   render() {
     return (
       <>
         <h1>My Favorite Books</h1>
-        <form>
-<fieldset>
+        <form onSubmit={this.addBook }>
+          <fieldset>
 
-<legend>Add Book :</legend>
-<input type="text" name="BookName" placeholder="Book Name"></input>
-<input type="text" name="Description" placeholder="Description"></input>
-<input type="text" name="Status" placeholder="Status"></input>
-<button type="submit">Add</button>
-
-
-</fieldset>
+            <legend>Add Book :</legend>
+            <input type="text" name="BookName" placeholder="Book Name"></input>
+            <input type="text" name="Description" placeholder="Description"></input>
+            <input type="text" name="Status" placeholder="Status"></input>
+            <button type="submit">Add</button>
 
 
+          </fieldset>
 
 
 
-        </form>
-        {this.state.FavBookArr.map(item => {
 
-          return (
-            <BookItems item={item} />
 
-          )
-        })
-        }
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </>
-    )
+      </form>
+      {
+      this.state.FavBookArr.map(item => {
+
+        return (
+          <BookItems item={item} 
+          
+          deleteBook ={this.deleteBook}
+          />
+
+        )
+      })
+    }
+    {/* <p>
+      This is a collection of my favorite books
+    </p> */}
+    </>
+  )
   }
 }
 
