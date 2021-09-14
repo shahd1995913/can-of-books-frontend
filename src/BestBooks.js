0c0dc550b982704be0823f48044461986c16521d
shahd1995913 <shahdalkhatib95@gmail.com>
2021-09-14 12:50:15 +0300

    update

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -38,10 +38,48 @@ class MyFavoriteBooks extends React.Component {
 
   }
 
+
+addBook=(event)=>{
+event.preventDefault();
+const{user}=this.props.auth0;
+const email =user.email;
+const obj={
+
+  BookName:event.target.BookName.value,
+  Description:event.target.Description.value,
+  Status:event.target.Status.value,
+  ownerEmail:email
+
+}
+
+
+// we need the POST to send data and the data secoure the data in body must to be not in the header
+//  POST-->Add  for security put the data inside the body no one can acess expect the authorized
+// When send data use POST the data will not be in Qury String Parameter QSP
+// the GET data will be exist in the QSP
+}
+
   render() {
     return (
       <>
         <h1>My Favorite Books</h1>
+        <form>
+<fieldset>
+
+<legend>Add Book :</legend>
+<input type="text" name="BookName" placeholder="Book Name"></input>
+<input type="text" name="Description" placeholder="Description"></input>
+<input type="text" name="Status" placeholder="Status"></input>
+<button type="submit">Add</button>
+
+
+</fieldset>
+
+
+
+
+
+        </form>
         {this.state.FavBookArr.map(item => {
 
           return (
